0314059854205249889e7a5db6d8dc0e85c3b50d
Cristian Pimentel <cristiantabarepimentel@gmail.com>
2025-12-10 13:06:10 -0400

    feature/create-user-module:Implementacion de la capa de persistencia, configuraciones centralizadas para esta tarea  y nuevas entidades soportado con DI

---
+++ Universal.UsersService.Api/Infrastructure/Persistence/UserRepository.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Universal.UsersService.Api.Domain.Entities;
+using Universal.UsersService.Api.Domain.Repositories;
+
+namespace Universal.UsersService.Api.Infrastructure.Persistence
+{
+    /// <summary>
+    /// Implementación de IUserRepository usando EF Core InMemory.
+    /// </summary>
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User?> GetByIdAsync(Guid id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task AddAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> EmailExistsAsync(string email)
+        {
+            return await _context.Users.AnyAsync(u => u.Email == email);
+        }
+    }
+}
--- Universal.UsersService.Api/Program.cs
+++ Universal.UsersService.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +7,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// Leer el nombre de la base de datos desde la configuración
+var dbName = builder.Configuration["InMemoryDatabaseName"] ?? "UserDb";
+builder.Services.AddDbContext<Universal.UsersService.Api.Infrastructure.Persistence.AppDbContext>(options =>
+    options.UseInMemoryDatabase(dbName));
+builder.Services.AddScoped<Universal.UsersService.Api.Domain.Repositories.IUserRepository, Universal.UsersService.Api.Infrastructure.Persistence.UserRepository>();
 
 
 var app = builder.Build();
